23cce7703da9e58f0aebf5bef54fa2746783939d
신관규 <soundbar91@gmail.com>
2026-03-03 14:11:21 +0900

    initial commit

---
+++ src/main/java/io/github/soundbar91/aipipeline/AiPipelineApplication.java
@@ -0,0 +1,13 @@
+package io.github.soundbar91.aipipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/soundbar91/aipipeline/AiPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.soundbar91.aipipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
